e02d89cefdd3e552625a8e3e2c306c35266b9cb2
lucasmzz <34027044+lucasmzz@users.noreply.github.com>
2021-07-06 16:56:26 +0200

    improvements-list Added additional comments in the APP component with the things I would add.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import "./styles.css";
 export default function App() {
   //THINGS TO ADD:
   // 1. Pagination for easing the result scrolling
+  // 2. Implement a debounced or throttled search to avoid overheating the API
+  // 3. Split the Search Results Card in a separate component.
+  // 4. Implement a results list where you can pick the one you are interested and navigate to it later on.
+
   return (
     <div className="App">
       <Search />
